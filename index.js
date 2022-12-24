c293f5790662bdeba79fe80fc4300b7d142820b0
Bryan Patrick <bpatrick@ou.edu>
2022-12-24 01:31:55 -0600

    WTF
    DENO functions that are DEPRECATED exist in the libraries used to observe keyboard input. GTFO I may as well write everything in FORTRAN

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import { notify } from "https://deno.land/x/notifier/mod.ts"
+import { keyboardInput, beginListeningToKeyboard, stopListeningToKeyboard } from "https://deno.land/x/deno_input/mod.ts";
 
-async function Pasteboard_Observer () {
+async function Input_Observer () {
+  // Keep the script alive
   setInterval(() => {}, 1 << 30)
-  
+
+  beginListeningToKeyboard();
+
+  for await ( const hit of keyboardInput() ) {
+      console.log( hit );
+      if ( hit.type == 'text' && hit.controlPressed && hit.text == 'c' )
+          break;
+  }
+
+  stopListeningToKeyboard();
+
   await notify("This is a title", "This is a message")
 }
+
+Input_Observer()
