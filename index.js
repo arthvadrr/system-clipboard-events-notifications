1a3758c95c010db1e4620680d0f88d7688a87a87
Bryan Patrick <bpatrick@ou.edu>
2022-12-26 22:51:01 -0600

    Update; we can hack the keyboard inputs

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import { notify } from "https://deno.land/x/notifier/mod.ts"
-import { keyboardInput, beginListeningToKeyboard, stopListeningToKeyboard } from "https://deno.land/x/deno_input/mod.ts";
+import { readKeypress } from "https://deno.land/x/keypress@0.0.8/mod.ts";
+Deno.setRaw = Deno.stdin.setRaw
 
 async function Input_Observer () {
   // Keep the script alive
   setInterval(() => {}, 1 << 30)
 
-  beginListeningToKeyboard();
+  for await (const keypress of readKeypress()) {
+    console.log(keypress);
 
-  for await ( const hit of keyboardInput() ) {
-      console.log( hit );
-      if ( hit.type == 'text' && hit.controlPressed && hit.text == 'c' )
-          break;
+    if (keypress.ctrlKey && keypress.key === 'c') {
+        Deno.exit(0);
+    }
   }
-
-  stopListeningToKeyboard();
 
   await notify("This is a title", "This is a message")
 }
